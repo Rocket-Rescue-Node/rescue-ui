195694f98e0b49607527b0d72acdda3caa87c577
Jacob Shufro <jacob@shuf.ro>
2024-02-03 21:17:45 -0500

    Use newer web3modal/wagmi package

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
-import { EthereumClient } from "@web3modal/ethereum";
-import { Web3Modal } from "@web3modal/react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { WagmiConfig } from "wagmi";
 
 import { App } from "./App";
-import { chains, config, walletConnectProjectId } from "./wagmi";
+import { config } from "./wagmi";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./theme";
-
-const ethereumClient = new EthereumClient(config, chains);
 
 const root = document.getElementById("root");
 if (!root) {
@@ -22,15 +18,6 @@ ReactDOM.createRoot(root).render(
         <CssBaseline />
         <App />
       </ThemeProvider>
-      <Web3Modal
-        projectId={walletConnectProjectId}
-        ethereumClient={ethereumClient}
-        themeMode={"dark"}
-        themeVariables={{
-          // Use our ETH-y secondary color as the accent.
-          "--w3m-accent-color": theme.palette.secondary.main,
-        }}
-      />
     </WagmiConfig>
   </React.StrictMode>,
 );
